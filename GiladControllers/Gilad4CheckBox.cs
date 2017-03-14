dc3a066f0cabe9a2bce161b19da58300f08c7be9
Gilad Reich <giladreichgr@gmail.com>
2017-03-15 00:34:09 +0100

    * Added new properties controls to the designer of the 4CheckBox Combo control.

--- GiladControllers/Gilad4CheckBox.cs
+++ GiladControllers/Gilad4CheckBox.cs
@@ -16,18 +16,20 @@ namespace GiladControllers
 
         private Color _labelForeColor          = Color.Black;
         private Color _labelForeColorDisabled  = Color.Gray;
-        private GiladCheckBox[] _checkBoxes;
+        private readonly GiladCheckBox[] _checkBoxes;
 
 
         public Gilad4CheckBox()
         {
             InitializeComponent();
-            this.cbOption1.pbCheckBox.Click += new EventHandler(cbOption1_CheckedChanged);
-            this.cbOption2.pbCheckBox.Click += new EventHandler(cbOption2_CheckedChanged);
-            this.cbOption3.pbCheckBox.Click += new EventHandler(cbOption3_CheckedChanged);
-            this.cbOption4.pbCheckBox.Click += new EventHandler(cbOption4_CheckedChanged);
-            _checkBoxes = new GiladCheckBox[4]{ cbOption1, cbOption2, cbOption3, cbOption4 };
+            RegisterSubEventHandlers();
+
+            _checkBoxes       = new GiladCheckBox[4]{ cbOption1, cbOption2, cbOption3, cbOption4 };
+            CheckBoxAutoSize  = false;
+            CheckBoxSize      = new Size(435, 55);
+            CheckBoxLabelSize = new Size(400, 40);
         }
+
 
         #region ----------------------------------- Properties -----------------------------------
         [Description("Hand Cursor will display while hovering button."), Category("~Custom Data")]
@@ -45,7 +47,6 @@ namespace GiladControllers
             }
         }
 
-
         [Description("Set/Get all the check boxes size."), Category("~Custom Data")]
         public Size CheckBoxSize
         {
@@ -56,6 +57,49 @@ namespace GiladControllers
                     return;
                 foreach (var checkBox in _checkBoxes)
                     checkBox.Size = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get all the check boxes Labels size."), Category("~Custom Data")]
+        public Size CheckBoxLabelSize
+        {
+            get { return cbOption1.lblCheckBox.Size; }
+            set
+            {
+                if (cbOption1.lblCheckBox.Size == value)
+                    return;
+                foreach (var checkBox in _checkBoxes)
+                    checkBox.lblCheckBox.Size = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get all the check boxes Font attributes."), Category("~Custom Data")]
+        public Font CheckBoxFont
+        {
+            get { return cbOption1.lblCheckBox.Font; }
+            set
+            {
+                if (Equals(cbOption1.lblCheckBox.Font, value))
+                    return;
+                foreach (var checkBox in _checkBoxes)
+                    checkBox.lblCheckBox.Font = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get AutSize of the labels. It's particle when text is too long so it jumps a row down."), Category("~Custom Data")]
+        public bool CheckBoxAutoSize
+        {
+            get { return cbOption1.lblCheckBox.AutoSize; }
+            set
+            {
+                if (cbOption1.lblCheckBox.AutoSize == value)
+                    return;
+                foreach (var checkBox in _checkBoxes)
+                    checkBox.lblCheckBox.AutoSize = value;
+                this.Invalidate();
             }
         }
 
@@ -89,6 +133,60 @@ namespace GiladControllers
         [Description("Returns the selected CheckBox index from the 4 boxes starting from 0 to 3.")]
         public int SelectedCheckBoxIndex => GetEnabledCheckBox();
 
+        [Description("Set/Get CheckBox 1 Text."), Category("~Custom Data")]
+        public string CheckBox1Text
+        {
+            get { return cbOption1.lblCheckBox.Text; }
+            set
+            {
+                if (cbOption1.lblCheckBox.Text == value)
+                    return;
+                cbOption1.lblCheckBox.Text = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get CheckBox 2 Text."), Category("~Custom Data")]
+        public string CheckBox2Text
+        {
+            get { return cbOption2.lblCheckBox.Text; }
+            set
+            {
+                if (cbOption2.lblCheckBox.Text == value)
+                    return;
+                cbOption2.lblCheckBox.Text = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get CheckBox 3 Text."), Category("~Custom Data")]
+        public string CheckBox3Text
+        {
+            get { return cbOption3.lblCheckBox.Text; }
+            set
+            {
+                if (cbOption3.lblCheckBox.Text == value)
+                    return;
+                cbOption3.lblCheckBox.Text = value;
+                this.Invalidate();
+            }
+        }
+
+        [Description("Set/Get CheckBox 4 Text."), Category("~Custom Data")]
+        public string CheckBox4Text
+        {
+            get { return cbOption4.lblCheckBox.Text; }
+            set
+            {
+                if (cbOption4.lblCheckBox.Text == value)
+                    return;
+                cbOption4.lblCheckBox.Text = value;
+                this.Invalidate();
+            }
+        }
+
+        #endregion ----------------------------------- Properties -----------------------------------
+
 
         /// <summary>
         /// Clears out all check marks from the check boxes.
@@ -102,58 +200,6 @@ namespace GiladControllers
             }
         }
 
-        [Description("Set/Get CheckBox 1 Text."), Category("~Custom Data")]
-        public string CheckBox1Text
-        {
-            get { return cbOption1.Text; }
-            set
-            {
-                if (cbOption1.Text == value)
-                    return;
-                cbOption1.Text = value;
-            }
-        }
-
-
-        [Description("Set/Get CheckBox 2 Text."), Category("~Custom Data")]
-        public string CheckBox2Text
-        {
-            get { return cbOption2.Text; }
-            set
-            {
-                if (cbOption2.Text == value)
-                    return;
-                cbOption2.Text = value;
-            }
-        }
-
-        [Description("Set/Get CheckBox 3 Text."), Category("~Custom Data")]
-        public string CheckBox3Text
-        {
-            get { return cbOption3.Text; }
-            set
-            {
-                if (cbOption3.Text == value)
-                    return;
-                cbOption3.Text = value;
-            }
-        }
-
-        [Description("Set/Get CheckBox 4 Text."), Category("~Custom Data")]
-        public string CheckBox4Text
-        {
-            get { return cbOption4.Text; }
-            set
-            {
-                if (cbOption4.Text == value)
-                    return;
-                cbOption4.Text = value;
-            }
-        }
-
-        #endregion ----------------------------------- Properties -----------------------------------
-
-
 
         private int GetEnabledCheckBox()
         {
@@ -166,6 +212,19 @@ namespace GiladControllers
 
 
         #region -------------------------------------- CheckBoxes Events ---------------------------------------
+        /// <summary>
+        /// When registering this in the desinger file, the c# designer will run into problems at some point.
+        /// This is why we do it seperatly for the sub components.
+        /// </summary>
+        private void RegisterSubEventHandlers()
+        {
+            this.cbOption1.pbCheckBox.Click += new EventHandler(cbOption1_CheckedChanged);
+            this.cbOption2.pbCheckBox.Click += new EventHandler(cbOption2_CheckedChanged);
+            this.cbOption3.pbCheckBox.Click += new EventHandler(cbOption3_CheckedChanged);
+            this.cbOption4.pbCheckBox.Click += new EventHandler(cbOption4_CheckedChanged);
+        }
+
+
         private void cbOption1_CheckedChanged(object sender, EventArgs e)
         {
             if (!cbOption1.CheckBoxEnabled)
